3677897a2835a10ef4605a2ac119155ca04f4ecb
nahrinoda <32439687+nahrinoda@users.noreply.github.com>
2018-08-07 00:40:33 -0400

    added Food2Fork API

---
+++ key.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.foodToFork = {
+  consumer_key: process.env.foodToFork_CONSUMER_KEY,
+  //consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  //access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  //access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.youTube = {
+  //id: process.env.SPOTIFY_ID,
+  //secret: process.env.SPOTIFY_SECRET
+};
